d50f51be5306763ec72c2f747ecda0b21499f24c
pakyrs <56080974+pakyrs@users.noreply.github.com>
2021-10-01 09:49:56 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,59 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install \
+magic-wormhole \ 
+matrix \
+curl \
+piglet \ 
+git \
+gnupg \ 
+vim \
+htop \
+iftop \
+ipcalc \
+iperf3 \
+mtr \
+mosh \
+ncdu \
+neofetch \ 
+nmap \
+p7zip \
+pwgen \
+ranger \
+screen \
+sshuttle \ 
+tldr \
+tmate \
+tmux \
+tree \
+wget \
+telnet \
+bpytop \
+
+—cask
+vivaldi \
+parsec \
+keepassxc \
+powershell \
+surfshark \
+teamviewer \
+transmission \
+visual-studio-code \
+vlc \
+zoom \
+biscuit \
+obsidian \
+authy \
+balenaetcher \
+appcleaner \
+nomachine \
+google-earth-pro \
+mpv \
+kitty \
+meld \
+lulu \
+microsoft-office \
+mactracker 
+
+None:
+pcloud
