58f9126ceba3d05a4de0cf6d877704bdfa065b93
pakyrs <56080974+pakyrs@users.noreply.github.com>
2021-12-06 18:02:13 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,58 +1,64 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#!/bin/bash
 
-brew install \
-magic-wormhole \ 
-matrix \
-curl \
-piglet \ 
-git \
-gnupg \ 
-vim \
-htop \
-iftop \
-ipcalc \
-iperf3 \
-mtr \
-mosh \
-ncdu \
-neofetch \ 
-nmap \
-p7zip \
-pwgen \
-ranger \
-screen \
-sshuttle \ 
-tldr \
-tmate \
-tmux \
-tree \
-wget \
-telnet \
-bpytop \
-the-unarchiver \
-vivaldi \
-parsec \
-keepassxc \
-powershell \
-surfshark \
-teamviewer \
-transmission \
-visual-studio-code \
-vlc \
-zoom \
-biscuit \
-obsidian \
-authy \
-balenaetcher \
-appcleaner \
-nomachine \
-google-earth-pro \
-mpv \
-kitty \
-meld \
-lulu \
-microsoft-office \
-mactracker 
 
-None:
-pcloud
+# INSTALL HOMEBREW
+# xcode-select --install
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install tmate
+brew install pwgen
+brew install vim
+brew install wget
+brew install nmap
+brew install putty
+brew install tldr
+brew install gpg
+brew install magic-wormhole
+brew install sshuttle
+brew install neofetch
+brew install matrix
+brew install curl
+brew install figlet
+brew install git
+brew install htop
+brew install ipcalc
+brew install iperf3
+brew install mtr
+brew install mosh
+brew install pwgen
+brew install screen
+brew install telnet
+brew install tmux 
+brew install tree
+brew install btop
+brew install --cask keepassxc
+brew install --cask Authy
+brew install --cask plex
+brew install --cask appcleaner
+brew install --cask biscuit
+brew install --cask surfshark
+brew install --cask vivaldi
+brew install --cask mactracker
+brew install --cask lulu
+brew install --cask android-file-transfer
+brew install --cask balenaetcher
+brew install --cask the-unarchiver
+brew install --cask vlc
+brew install --cask microsoft-teams
+brew install --cask teamviewer
+brew install --cask calibre
+brew install --cask transmission
+brew install --cask gemini
+brew install --cask maccy
+brew install --cask powershell
+brew install --cask screens
+brew install --cask wine-stable
+brew install --cask visual-studio-code
+brew install --cask powershell
+brew install --cask obsidian
+brew install --cask deskreen
+brew install --cask transmission
+
+
+# None:
+# pcloud
