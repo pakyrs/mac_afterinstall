457c6b349322a72cea19cfa550b454070515981f
pakyrs <56080974+pakyrs@users.noreply.github.com>
2022-04-29 23:02:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,13 +29,11 @@ brew install mtr
 brew install mosh
 brew install pwgen
 brew install telnet
-brew install tmux 
 brew install tree
-brew install btop
+brew install croc
 brew install yadm
 brew install cmatrix
 brew install speedtest-cli
-brew install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize suspicious-package apparency quicklookase qlvideo
 brew install pock
 brew install ncdu
 brew install --cask keepassxc
@@ -52,15 +50,11 @@ brew install --cask vivaldi
 brew install --cask mactracker
 brew install --cask lulu
 brew install --cask android-file-transfer
-brew install --cask balenaetcher
 brew install --cask the-unarchiver
 brew install --cask vlc
-#brew install --cask microsoft-teams
 brew install --cask teamviewer
 brew install --cask anydesk
 brew install --cask calibre
-brew install --cask transmission
-#brew install --cask gemini
 brew install --cask maccy
 brew install --cask powershell
 brew install --cask screens
@@ -68,12 +62,8 @@ brew install --cask screens
 brew install --cask visual-studio-code
 brew install --cask powershell
 brew install --cask obsidian
-#brew install --cask deskreen
 brew install --cask transmission
 brew install --cask microsoft-remote-desktop
-brew install --cask anydesk
-brew install --cask teamviewer
-#brew install --cask bluestacks
 
 # None:
 # pcloud
